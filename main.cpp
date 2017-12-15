9341004bb8037b94bc866a528dfc8dfcf2bb329f
Lassi Eloranta <lassi.eloranta@gmail.com>
2017-12-15 23:37:37 +0200

    Lisätty komentorivi-vipu tiedostoon tallennukselle.

--- main.cpp
+++ main.cpp
@@ -4,24 +4,34 @@
 
 using namespace std;
 
+struct caesar {
+    unsigned int key;
+    string text;
+} salaus, plain, ciphered;
+
 // Prototyypataan funktiot
 int argChecker(int& countOfArgc, char *valueofArgv);
-string cipherText(int& key, string& plainText);
-void savetoFile(string& plain, int& key, string& ciphered);
+string cipherText(unsigned int& key, string& plainText);
+void savetoFile(string& plain, unsigned int& key, string& ciphered, char& saveFlag);
 
 int main(int argc, char **argv) {
     // Estetään segfault jossei komentoriviargumenttia ole annettu
+    char saveFlag;
     if (argv[1] == nullptr)
     {
         cout << "Komentoriviargumentti puuttuu. Anna luku väliltä 1-26." << endl;
         return(1);
     }
+    if (argv[2] == nullptr)
+    {
+        saveFlag = 'N';
+    }
+    else
+    {
+        saveFlag = *argv[2];
+    }
+    cout << saveFlag;
     // caesar-tietue salaamattoman & salatun sanan sekä salausavaimen säilytykseen
-    struct caesar {
-        int key;
-        string text;
-    } salaus, plain, ciphered;
-    
     salaus.key = argChecker(argc, argv[1]);
     cout << "Alkuperäinen teksti: ";
     getline (cin, plain.text);
@@ -29,7 +39,7 @@ int main(int argc, char **argv) {
     ciphered.text = cipherText(salaus.key, plain.text);
     cout << ciphered.text;
     cout << endl;
-	savetoFile(plain.text, salaus.key, ciphered.text);
+	savetoFile(plain.text, salaus.key, ciphered.text, saveFlag);
     return(0);
 }
 /**
@@ -41,18 +51,18 @@ int main(int argc, char **argv) {
 int argChecker(int& countOfArgc, char *valueofArgv)
 {
     // Muutetaan komentorivisyöte string-tyypistä kokonaisluvuksi
-    int argvToInt = atoi(valueofArgv);
+    unsigned int argvToInt = atoi(valueofArgv);
     if(countOfArgc < 2)
     {
         cout << "Komentoriviargumentti puuttuu!" << endl;
         exit(1);
     }
-    else if(countOfArgc > 2)
+    else if(countOfArgc > 3)
     {
         cout << "Komentoriviargumentteja on liikaa! " << "Anna ainoastaan numero väliltä 1-26." << endl;
         exit(1);
     }
-    if(argvToInt <=0 || argvToInt > 26)
+    if(argvToInt == 0 || argvToInt > 26)
     {
         cout << "Anna ainoastaan numero väliltä 1-26!" << endl;
         exit(1);
@@ -65,7 +75,7 @@ int argChecker(int& countOfArgc, char *valueofArgv)
  * @param plainText Selkokielinen sana/lause, joka halutaan salata
  * @return Palauttaa string-tyypin muuttujan, jossa salattu versio sanasta
  */
-string cipherText(int& key, string& plainText)
+string cipherText(unsigned int& key, string& plainText)
 {
     string ciphered;
     // Käydään salaamaton sana läpi
@@ -76,7 +86,7 @@ string cipherText(int& key, string& plainText)
         {
             // Muutetaan merkki aakkos-indeksiin
             char cipher = tolower(c) - 'a';
-            
+
             // Siirretään kirjainta aakkos-indeksillä salaus-avaimen luvun verran
             // Sekä otetaan jakojäännös luvulla 26, jotta Z-kirjaimen jälkeen pyörähdetään takaisin A-kirjaimeen
             cipher = cipher + key;
@@ -109,16 +119,19 @@ string cipherText(int& key, string& plainText)
  * @param key Salausavain
  * @param ciphered Salattu teksti
  */
-void savetoFile(string& plainText, int& key, string& ciphered)
+void savetoFile(string& plainText, unsigned int& key, string& ciphered, char& saveFlag)
 {
-    // Luodaan outputstream-objekti outfile
-	ofstream outfile;
-    // Avataan sanaparit.txt-tiedosto append-tilassa, jolloin tallennettu tieto menee aina uudelle omalle rivilleen.
-	outfile.open("sanaparit.txt", ios_base::app);
-	
-    // Kirjoitetaan tieto tiedostoon ja suljetaan tiedosto
-	cout << "Kirjoitetaan sanapari tiedostoon..." << endl;
-	outfile << plainText << " + " << key << " = " << ciphered << endl;
-	cout << "Kirjoitus valmis." << endl;
-	outfile.close();
+    if(saveFlag == 'Y')
+    {
+        // Luodaan outputstream-objekti outfile
+        ofstream outfile;
+        // Avataan sanaparit.txt-tiedosto append-tilassa, jolloin tallennettu tieto menee aina uudelle omalle rivilleen.
+        outfile.open("sanaparit.txt", ios_base::app);
+
+        // Kirjoitetaan tieto tiedostoon ja suljetaan tiedosto
+        cout << "Kirjoitetaan sanapari tiedostoon..." << endl;
+        outfile << plainText << " + " << key << " = " << ciphered << endl;
+        cout << "Kirjoitus valmis." << endl;
+        outfile.close();
+    }
 }
